605a97f449a96456631714283dc204b68db171bc
5om3Bod1 <brandonxiedesigner@gmail.com>
2024-09-11 11:21:12 -0400

    hi

---
+++ Assets/mouseScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseScript : MonoBehaviour
+{
+    public int count;
+
+    public void damageTaken()
+    {
+        count++;
+    }
+}
