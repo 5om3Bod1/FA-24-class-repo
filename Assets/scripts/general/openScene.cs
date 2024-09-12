ece5594a361f8b65403b4db8bdde55af1bafe106
5om3Bod1 <brandonxiedesigner@gmail.com>
2024-09-12 09:21:45 -0400

    bye

---
+++ Assets/scripts/general/openScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class openScene : MonoBehaviour
+{
+    public int gameOne;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Hello World");
+            SceneManager.LoadScene(gameOne, LoadSceneMode.Single);
+        }
+    }
+}
